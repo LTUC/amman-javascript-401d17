e8311723db8914ecb9175d29e96d48e97bacf990
waleedafifi90 <waleedafifi.blog@gmail.com>
2023-07-04 12:51:17 +0300

    Class 9 demo - review

---
+++ class-09/demo/src/models/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const clothesModel = require('./clothes/model.js');
+const foodModel = require('./food/model.js');
+const userModel = require('./user/model.js');
+const Collection = require('./data-collection.js');
+
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
+
+const sequelize = new Sequelize(DATABASE_URL);
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
+const users = userModel(sequelize, DataTypes);
+
+module.exports = {
+  db: sequelize,
+  food: new Collection(food),
+  clothes: new Collection(clothes),
+  users: new Collection(users),
+  userModel: userModel(sequelize, DataTypes)
+};
